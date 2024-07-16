256ab045d99eeadde282bb6df243174f12797524
nikhilreddykunta <kunta.nike.n@gmail.com>
2024-07-16 19:32:45 +0530

    implement send 200 status

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,8 +17,15 @@ public class Main {
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
 
-       serverSocket.accept(); // Wait for connection from client.
+       Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
+
+//       clientSocket.getOutputStream();
+       PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+       out.write("HTTP/1.1 200 OK\\r\\n\\r\\n");
+
+       serverSocket.close();
+       clientSocket.close();
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
